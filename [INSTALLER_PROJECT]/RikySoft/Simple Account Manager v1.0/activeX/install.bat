291380d2f17572a296d28bc5bce61b4081b6bfe7
Rizky Khapidsyah <45317201+RizkyKhapidsyah@users.noreply.github.com>
2019-02-05 19:57:16 +0700

    Add files via upload

---
+++ [INSTALLER_PROJECT]/RikySoft/Simple Account Manager v1.0/activeX/install.bat
@@ -0,0 +1,28 @@
+copy AeroSuite.ocx C:\windows\system32\AeroSuite.ocx
+copy comdlg32.ocx C:\windows\system32\comdlg32.ocx
+copy dcButton.ocx C:\windows\system32\dcButton.ocx
+copy HookMenu.ocx C:\windows\system32\HookMenu.ocx
+copy MSADODC.OCX C:\windows\system32\MSADODC.OCX
+copy MSCAL.OCX C:\windows\system32\MSCAL.OCX
+copy MSCOMCTL.OCX C:\windows\system32\MSCOMCTL.OCX
+copy MSDATGRD.OCX C:\windows\system32\MSDATGRD.OCX
+copy TPhoto.ocx C:\windows\system32\TPhoto.ocx
+copy XPControls.ocx C:\windows\system32\XPControls.ocx
+copy xTab.ocx C:\windows\system32\xTab.ocx
+copy XP_Engine.ocx C:\windows\system32\XP_Engine.ocx
+
+regsvr32 AeroSuite.ocx
+regsvr32 comdlg32.ocx
+regsvr32 dcButton.ocx
+regsvr32 HookMenu.ocx
+regsvr32 MSADODC.OCX
+regsvr32 MSCAL.OCX
+regsvr32 MSCOMCTL.OCX
+regsvr32 MSDATGRD.OCX
+regsvr32 TPhoto.ocx
+regsvr32 XPControls.ocx
+regsvr32 xTab.ocx
+regsvr32 XP_Engine.ocx
+
+pause
+
